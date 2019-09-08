92ea7882de9bcb7b5f85498c1399153ea204a384
eugnsp <evgeny.sg@gmail.com>
2019-09-08 20:10:21 +0300

    02_06_multiresolution_analysis

--- src/02_06_multiresolution_analysis.cpp
+++ src/02_06_multiresolution_analysis.cpp
@@ -18,81 +18,44 @@ This file is covered by the LICENSE file in the root of this project.
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 #include <vector>
 
 struct Haar_wavelet
 {
-	static constexpr std::size_t n_forward = 2;
-	static constexpr std::ptrdiff_t forward_shift = 0;
-	static constexpr std::size_t n_backward = 1;
+	static constexpr double forward[2][2] = {{M_SQRT1_2, M_SQRT1_2}, {M_SQRT1_2, -M_SQRT1_2}};
+
 	static constexpr std::ptrdiff_t backward_shift = 0;
-
-	template<typename T>
-	static void forward(T& out_c, T& out_d, T in_c0, T in_c1)
-	{
-		out_c = M_SQRT1_2 * (in_c0 + in_c1);
-		out_d = M_SQRT1_2 * (in_c0 - in_c1);
-	}
-
-	template<typename T>
-	static void backward(T& out_c0, T& out_c1, T in_c, T in_d)
-	{
-		out_c0 = M_SQRT1_2 * (in_c + in_d);
-		out_c1 = M_SQRT1_2 * (in_c - in_d);
-	}
+	static constexpr double backward_c[2][1] = {{M_SQRT1_2}, {M_SQRT1_2}};
+	static constexpr double backward_d[2][1] = {{M_SQRT1_2}, {-M_SQRT1_2}};
 };
 
 struct Schauder_wavelet
 {
-	static constexpr std::size_t n_forward = 3;
-	static constexpr std::ptrdiff_t forward_shift = 0;
-	static constexpr std::size_t n_backward = 2;
+	static constexpr double forward[2][3] = {{M_SQRT2, 0, 0}, {-M_SQRT1_2, M_SQRT2, -M_SQRT1_2}};
+
 	static constexpr std::ptrdiff_t backward_shift = 0;
-
-	template<typename T>
-	static void forward(T& out_c, T& out_d, T in_c0, T in_c1, T in_c2)
-	{
-		out_c = M_SQRT2 * in_c0;
-		out_d = M_SQRT2 * (in_c1 - .5 * (in_c0 + in_c2));
-	}
-
-	template<typename T>
-	static void backward(T& out_c0, T& out_c1, T in_c0, T in_c1, T in_d0, T)
-	{
-		out_c0 = M_SQRT1_2 * in_c0;
-		out_c1 = M_SQRT1_2 * (in_d0 + .5 * (in_c0 + in_c1));
-	}
+	static constexpr double backward_c[2][2] = {{M_SQRT1_2, 0}, {M_SQRT1_2 / 2, M_SQRT1_2 / 2}};
+	static constexpr double backward_d[2][2] = {{0, 0}, {M_SQRT1_2, 0}};
 };
 
 struct Daubechies_wavelet
 {
-	static constexpr std::size_t n_forward = 4;
-	static constexpr std::ptrdiff_t forward_shift = 0;
-	static constexpr std::size_t n_backward = 2;
-	static constexpr std::ptrdiff_t backward_shift = -1;
-
-	template<typename T>
-	static void forward(T& out_c, T& out_d, T in_c0, T in_c1, T in_c2, T in_c3)
-	{
-		out_c = c0 * in_c0 + c1 * in_c1 + c2 * in_c2 + c3 * in_c3;
-		out_d = c3 * in_c0 - c2 * in_c1 + c1 * in_c2 - c0 * in_c3;
-	}
-
-	template<typename T>
-	static void backward(T& out_c0, T& out_c1, T in_cm1, T in_c0, T in_dm1, T in_d0)
-	{
-		out_c0 = c2 * in_cm1 + c1 * in_dm1 + c0 * in_c0 + c3 * in_d0;
-		out_c1 = c3 * in_cm1 - c0 * in_dm1 + c1 * in_c0 - c2 * in_d0;
-	}
-
 private:
 	static constexpr auto sqrt3 = 1.732050807568877;
 	static constexpr auto c0 = (1 + sqrt3) / (4 * M_SQRT2);
 	static constexpr auto c1 = (3 + sqrt3) / (4 * M_SQRT2);
 	static constexpr auto c2 = (3 - sqrt3) / (4 * M_SQRT2);
 	static constexpr auto c3 = (1 - sqrt3) / (4 * M_SQRT2);
+
+public:
+	static constexpr double forward[2][4] = {{c0, c1, c2, c3}, {c3, -c2, c1, -c0}};
+
+	static constexpr std::ptrdiff_t backward_shift = -1;
+	static constexpr double backward_c[2][2] = {{c2, c0}, {c3, c1}};
+	static constexpr double backward_d[2][2] = {{c1, c3}, {-c0, -c2}};
 };
 
 template<typename T>
@@ -125,16 +88,19 @@ void zero_small_coeffs(Vec& vec, T max_value)
 template<class Fn, typename T>
 auto fn_sample(Fn&& fn, std::size_t n, T x_min, T x_max)
 {
+	std::vector<T> xs;
 	std::vector<std::invoke_result_t<Fn, T>> values;
+	xs.reserve(n);
 	values.reserve(n);
 
 	for (std::size_t i = 0; i < n; ++i)
 	{
 		const auto x = x_min + (x_max - x_min) * i / n;
+		xs.push_back(x);
 		values.push_back(fn(x));
 	}
 
-	return values;
+	return std::make_pair(xs, values);
 }
 
 template<class Wavelet, class In, class Out, std::size_t... is>
@@ -150,7 +116,10 @@ void wavelet_forward_impl(const In& in, Out& out, std::index_sequence<is...>)
 	{
 		const auto n2 = n / 2;
 		for (std::size_t k = 0; k < n2; ++k)
-			Wavelet::forward(tmp[k], tmp[n2 + k], out[(2 * k + is + Wavelet::forward_shift) % n]...);
+		{
+			tmp[k] = ((Wavelet::forward[0][is] * out[(2 * k + is) % n]) + ...);
+			tmp[n2 + k] = ((Wavelet::forward[1][is] * out[(2 * k + is) % n]) + ...);
+		}
 		std::copy_n(tmp.begin(), n, out.begin());
 		n = n2;
 	}
@@ -171,8 +140,12 @@ void wavelet_backward_impl(const In& in, Out& out, std::index_sequence<is...>)
 		const auto j2 = 2 * j;
 		std::copy_n(out.begin(), j2, tmp.begin());
 		for (std::size_t k = 0; k < j; ++k)
-			Wavelet::backward(out[2 * k], out[2 * k + 1], tmp[(k + is + Wavelet::backward_shift) % j]...,
-				tmp[j + (k + is + Wavelet::backward_shift) % j]...);
+		{
+			out[2 * k] = ((Wavelet::backward_c[0][is] * tmp[(k + is + Wavelet::backward_shift) % j]) + ...) +
+						 ((Wavelet::backward_d[0][is] * tmp[j + (k + is + Wavelet::backward_shift) % j]) + ...);
+			out[2 * k + 1] = ((Wavelet::backward_c[1][is] * tmp[(k + is + Wavelet::backward_shift) % j]) + ...) +
+							 ((Wavelet::backward_d[1][is] * tmp[j + (k + is + Wavelet::backward_shift) % j]) + ...);
+		}
 		j = j2;
 	}
 }
@@ -180,13 +153,15 @@ void wavelet_backward_impl(const In& in, Out& out, std::index_sequence<is...>)
 template<class Wavelet, class In, class Out>
 void wavelet_forward(const In& in, Out& out)
 {
-	wavelet_forward_impl<Wavelet>(in, out, std::make_index_sequence<Wavelet::n_forward>{});
+	constexpr auto n = std::extent_v<decltype(Wavelet::forward), 1>;
+	wavelet_forward_impl<Wavelet>(in, out, std::make_index_sequence<n>{});
 }
 
 template<class Wavelet, class In, class Out>
 void wavelet_backward(const In& in, Out& out)
 {
-	wavelet_backward_impl<Wavelet>(in, out, std::make_index_sequence<Wavelet::n_backward>{});
+	constexpr auto n = std::extent_v<decltype(Wavelet::backward_c), 1>;
+	wavelet_backward_impl<Wavelet>(in, out, std::make_index_sequence<n>{});
 }
 
 template<class Wavelet>
@@ -202,11 +177,11 @@ void wavelet_test(std::string file_name, double eps1, double eps2)
 	const auto c = fn_sample(f, n, x_min, x_max);
 
 	std::vector<double> d;
-	wavelet_forward<Wavelet>(c, d);
+	wavelet_forward<Wavelet>(c.second, d);
 
 	std::vector<double> c0;
 	wavelet_backward<Wavelet>(d, c0);
-	assert(are_equal(c, c0));
+	assert(are_equal(c.second, c0));
 
 	std::vector<double> c1;
 	zero_small_coeffs(d, eps1);
@@ -218,10 +193,7 @@ void wavelet_test(std::string file_name, double eps1, double eps2)
 
 	std::ofstream file(file_name);
 	for (std::size_t i = 0; i < n; ++i)
-	{
-		const auto x = x_min + (x_max - x_min) * i / n;
-		file << x << ' ' << c[i] << ' ' << c1[i] << ' ' << c2[i] << std::endl;
-	}
+		file << c.first[i] << ' ' << c.second[i] << ' ' << c1[i] << ' ' << c2[i] << std::endl;
 }
 
 int main()
